d6bff70f5699fd6e9d9b9c509acc43a69f62ad30
Iryna Yehorova <iryna.yehorova@faceit.com.ua>
2022-04-05 10:31:57 +0300

    add initial style, list of jobs

--- src/App.vue
+++ src/App.vue
@@ -2,55 +2,38 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="green"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <span class="d-flex align-center">
+       Job App
+      </span>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href=""
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">Arbeit now</span>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <Main />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Main from './views/Main';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Main,
   },
 
   data: () => ({
---
+++ src/views/Main.vue
@@ -0,0 +1,42 @@
+<template>
+<v-card
+    class="mx-auto"
+    tile
+  >
+    <v-list shaped>
+      <v-subheader>Job Applications</v-subheader>
+      <v-list-item-group
+        v-model="selectedItem"
+        color="green"
+      >
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+    data: () => ({
+    items: [],
+    selectedItem: {}
+  }),
+  created() {
+      this.getJobs();
+  },
+  methods: {
+      async getJobs() {
+        fetch('https://www.arbeitnow.com/api/job-board-api')
+          .then(response => response.json())
+          .then(data => this.items = data.data)
+      },
+  }
+}
+</script>
